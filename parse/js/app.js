cb2e0bf832c0807920900bf3337124efd431685c
Hai Nguyen <hai427@uw.edu>
2015-10-22 16:51:30 -0700

    10/22 Notes

--- parse/js/app.js
+++ parse/js/app.js
@@ -2,16 +2,18 @@
     script for the index.html file
 */
 
-Parse.initialize("HwGkNK09YRPy3ZajicPwpZMfX9vqCyc4ghFl2eh7", "14BQF3zAPvaOR1sh6aEzXX5Wk1LTnBFQopjr1Rbj");;
+Parse.initialize("rnPVLff4nz9OW4qu9GnkdD18TV4AzzxfLducfGQh", "x62CpPBIqQwvj2nW1eRSo50VWvUMxFSoyJG8NuVB");
 
 $(function() {
     'use strict';
 
     //new Task class for parse
     var Task = Parse.Object.extend('Task');
+
     //new query that will return all tasks ordered by createAt
     var tasksQuery = new Parse.Query(Task);
     tasksQuery.ascending('createdAt');
+    tasksQuery.notEqualTo('done', true);
 
     //reference to the task list element
     var tasksList = $('#tasks-list');
@@ -21,6 +23,9 @@ $(function() {
 
     //current set of tasks
     var tasks = [];
+
+    //reference to our rating element
+    var ratingElem = $('#rating');
 
     function displayError(err) {
         errorMessage.text(err.message);
@@ -54,11 +59,29 @@ $(function() {
     function renderTasks() {
         tasksList.empty();
         tasks.forEach(function(task) {
-            $(document.createElement('li'))
-                .text(task.get('title'))
-                .appendTo(tasksList);
+            var li = $(document.createElement('li'))
+                .html(task.get('title'))
+                .addClass(task.get('done') ? 'completed-task' : '')
+                .appendTo(tasksList)
+                .click(function() {
+                    task.set('done', !task.get('done'))
+                    task.save().then(renderTasks, displayError);
+                });
+
+            $(document.createElement('span'))
+                .raty({readOnly: true,
+                    score: (task.get('rating') || 0),
+                    hints: ['crap', 'awful', 'ok', 'nice', 'awesome']})
+                .appendTo(li);
         });
     }
+
+    function showMessage(message) {
+        message = message || 'Hello';
+        alert(message);
+    }
+
+    showMessage('World')
 
     //when the user submits the new task form...
     $('#new-task-form').submit(function(evt) {
@@ -68,8 +91,10 @@ $(function() {
         var title = titleInput.val();
         var task = new Task();
         task.set('title',title);
+        task.set('rating', ratingElem.raty('score'));
         task.save().then(fetchTasks, displayError).then(function() {
-            titleInput.val();
+            titleInput.val('');
+            ratingElem.raty('set',{});
         });
 
         return false;
@@ -78,5 +103,9 @@ $(function() {
     //go and fetch tasks from Parse
     fetchTasks();
 
+    ratingElem.raty();
+
+    //refetch the tasks every so often
+    //to get new tasks created by others
     window.setInterval(fetchTasks, 3000);
 });
