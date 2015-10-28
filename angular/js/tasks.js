1dda6ad5593ceeb3f906bd72f1a04b1f2dfba93d
Hai Nguyen <hai427@uw.edu>
2015-10-27 17:16:44 -0700

    10/27 angular notes

--- angular/js/tasks.js
+++ angular/js/tasks.js
@@ -2,3 +2,35 @@
     script for the tasks.html file 
 */
 
+angular.module('Tasks', [])
+    .constant('taskKey', 'tasks')
+    .controller('TasksController', function($scope, taskKey) {
+        'use strict';
+
+        //initialize tasks property on the scope to an empty array
+        $scope.tasks = angular.fromJson(localStorage.getItem(taskKey)) || [];
+        //initialize newTask to an empty object
+        $scope.newTask = {};
+
+        function saveTasks() {
+            localStorage.setItem(taskKey, angular.toJson($scope.tasks))
+        }
+
+        //add a function to add newTask to the array
+        $scope.addTask = function() {
+            //push the current value of the newTask into the tasks array
+            $scope.tasks.push($scope.newTask);
+
+            //save the tasks
+            saveTasks();
+
+            //reset newTask to an empty object
+            $scope.newTask = {};
+        }
+
+        //function to toggle task done state
+        $scope.toggleDone = function(task) {
+            task.done = !task.done;
+            saveTasks();
+        };
+    });
