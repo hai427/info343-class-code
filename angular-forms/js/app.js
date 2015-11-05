51d779084ecf8b89d82b518fd026104e22e7adc6
Hai Nguyen <hai427@uw.edu>
2015-11-05 14:32:01 -0800

    11/5 revised notes

--- angular-forms/js/app.js
+++ angular-forms/js/app.js
@@ -1,26 +1,27 @@
 /*
-    script file for the index.html page
-*/
+ script file for the index.html page
+ */
 
+// The array contains strings from the ui-router module
 angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'])
     .constant('storageKey', 'contacts-list')
     .factory('contacts', function(uuid, localStorageService, storageKey) {
         return [{
             id: 'default-delete-me',
-            fname: 'HAAYYYYEEE',
-            lname: 'Neuen',
-            phone: '123-456-7890',
-            dob: '1/23/4567'
+            fname: 'John',
+            lname: 'S-117',
+            phone: '206-555-1212',
+            dob: '3/28/2255'
         }];
     })
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('list', {
+            .state('list', { // Name called list
                 url: '/contacts',
-                templateUrl: 'views/contacts-list.html',
+                templateUrl: 'views/contacts-list.html', // injects into empty <div>
                 controller: 'ContactsController'
             })
-            .state('detail', {
+            .state('detail', { // Name called detail
                 url: '/contacts/:id',
                 templateUrl: 'views/contact-detail.html',
                 controller: 'ContactDetailController'
@@ -30,20 +31,18 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
                 templateUrl: 'views/edit-contact.html',
                 controller: 'EditContactController'
             });
-
         $urlRouterProvider.otherwise('/contacts');
     })
-    .controller('ContactsController', function($scope, contacts) {
+    .controller('ContactsController', function($scope, contacts) { // Takes factory param 'contacts'
         $scope.contacts = contacts;
     })
-    .controller('ContactDetailController', function($scope, $stateParams,
-                                                    $state, contacts) {
-        $scope.contact = contacts.find(function (contact) {
-            return contact.id === $stateParams.id;
+    .controller('ContactDetailController', function($scope, $stateParams, $state, contacts) {
+        $scope.contact = contacts.find(function(contact) {
+            return contact.id === $stateParams.id; // We used :id in the second .state() if :foo we would use .foo instead of .id
         });
+
     })
-    .controller('EditContactController', function($scope, $stateParams,
-                                                    $state, contacts) {
+    .controller('EditContactController', function($scope, $stateParams, $state, contacts) {
         var existingContact = contacts.find(function(contact) {
             return contact.id === $stateParams.id;
         });
@@ -51,7 +50,7 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         $scope.contact = angular.copy(existingContact);
 
         $scope.save = function() {
-            angular.copy($scope.contact, existingContact);
-            $state.go("list");
+            angular.copy($scope.contact, existingContact); // Left is source, right is destination
+            $state.go('list');
         }
     });
